5ede8236a08dc8fe803aefd07516c24e2b57c63a
Robbie Christhover <krisyeski@gmail.com>
2021-10-24 23:41:55 +0700

    design detail movie

---
+++ app/src/main/java/com/dev/divig/moviereviewsapp/data/model/DetailMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.divig.moviereviewsapp.data.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.divig.moviereviewsapp.R
+
+class DetailMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}
