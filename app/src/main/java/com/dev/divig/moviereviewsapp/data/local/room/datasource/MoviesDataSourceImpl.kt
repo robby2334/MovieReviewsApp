d9ac91b467c5d72d39b53e673e4d8d24c93060da
Divig-dev <mrifkinuryanta@gmail.com>
2021-10-28 11:18:51 +0700

    Fix DataSource & DAO

--- app/src/main/java/com/dev/divig/moviereviewsapp/data/local/room/datasource/MoviesDataSourceImpl.kt
+++ app/src/main/java/com/dev/divig/moviereviewsapp/data/local/room/datasource/MoviesDataSourceImpl.kt
@@ -5,12 +5,8 @@ import com.dev.divig.moviereviewsapp.data.model.MovieEntity
 import com.dev.divig.moviereviewsapp.data.model.ReviewEntity
 
 class MoviesDataSourceImpl(private val dao: MoviesDao) : MoviesDataSource {
-    override suspend fun insertMovies(movies: List<MovieEntity>): Long {
-        var result = 0L
-        movies.forEach {
-            result = dao.insertMovies(it)
-        }
-        return result
+    override suspend fun insertMovies(movies: List<MovieEntity>): Int {
+        return dao.insertMovies(movies).size
     }
 
     override suspend fun getMovies(): List<MovieEntity> {
